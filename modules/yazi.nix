c5d7a171e5673c4ba754d78f39b299b456b8c586
sergioahp <sergioahp@proton.me>
2026-07-15 20:27:40 -0600

    Wrap yazi with repo-managed settings/keymap and dragon-drop keybinds
    Exposes yazi.settings / yazi.keymap perSystem options, renders them
    to TOML, and wraps the yazi binary with YAZI_CONFIG_HOME pointing at
    the generated config.

--- modules/yazi.nix
+++ modules/yazi.nix
@@ -1,6 +1,17 @@
-{ ... }: {
-  perSystem = { pkgs, ... }: {
-    zsh.initExtra = /* zsh */ ''
+{ lib, ... }: {
+  perSystem = { config, pkgs, ... }: {
+    options = {
+      yazi.settings = lib.mkOption {
+        type = lib.types.attrs;
+        default = { };
+        description = "Passed as a lua table to the plugin's setup function";
+      };
+      yazi.keymap = lib.mkOption {
+        type = lib.types.attrs;
+        default = { };
+      };
+    };
+    config.zsh.initExtra = /* zsh */ ''
       function y() {
         local tmp
         tmp="$(mktemp -t yazi-cwd.XXXXX)"
@@ -11,5 +22,51 @@
         rm -f -- "$tmp"
       }
     '';
+    config.yazi.keymap = {
+      mgr.prepend_keymap = [
+        {
+          on = "<C-d>";
+          run = ''shell -- ${pkgs.dragon-drop}/bin/dragon-drop "$@";'';
+          desc = "Dragon drop";
+        }
+        {
+          on = "<C-o>";
+          run = "back";
+          desc = "Dir history back";
+        }
+        {
+          on = "<C-i>";
+          run = "forward";
+          desc = "Dir history forward";
+        }
+      ];
+    };
+    config.packages = {
+      yazi = let
+        pkg = pkgs.yazi;
+        format = pkgs.formats.toml {};
+        yazi-toml = format.generate "yazi.toml" config.yazi.settings;
+        keymap-toml = format.generate "keymap.toml" config.yazi.keymap;
+        yazi-config-home = pkgs.linkFarm "yazi-config-home" [
+          {
+            name = "yazi.toml";
+            path = yazi-toml;
+          }
+          {
+            name = "keymap.toml";
+            path = keymap-toml;
+          }
+        ];
+      in
+        pkgs.symlinkJoin {
+          inherit (pkg) name meta;
+          nativeBuildInputs = [ pkgs.makeWrapper ];
+          paths = [ pkg ];
+          postBuild = ''
+            wrapProgram $out/bin/yazi \
+              --set YAZI_CONFIG_HOME ${yazi-config-home}
+            '';
+        };
+    };
   };
 }
